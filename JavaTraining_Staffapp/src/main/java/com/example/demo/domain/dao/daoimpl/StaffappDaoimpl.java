587a6fc73b85759b91e303c958c3a842c10a990c
HayatoKaji <hayabo.program@gmail.com>
2021-12-13 18:58:44 +0900

    j-touyama Userクラスで不要なものを削除しDaoimplで定義

--- JavaTraining_Staffapp/src/main/java/com/example/demo/domain/dao/daoimpl/StaffappDaoimpl.java
+++ JavaTraining_Staffapp/src/main/java/com/example/demo/domain/dao/daoimpl/StaffappDaoimpl.java
@@ -9,6 +9,7 @@ import com.example.demo.domain.model.User;
 
 @Repository
 public class TrainingDaoimpl implements TrainingDao {
+	private static final USER = "system_user";　
 
 	@Autowired
 	private JdbcTemplate jdbc;
@@ -20,6 +21,11 @@ public class TrainingDaoimpl implements TrainingDao {
 				user.getFirst_name_romaji(), user.getStaff_department(), user.getProject_type(), user.getJoined_year(),
 				user.getNew_glad_flg(), user.getCreated_by(), user.getUpdated_by(), user.getCreated_at(),
 				user.getUpdated_at());
+		
+		long millis = System.currentTimeMillis();//Userクラスで削除したものをDBに登録するために定義
+		Timestamp nowTime = new Timestamp(millis);　//「メソッド変数」このメソッド内でしか使用しない。
+		・・・
+		jdbc.update(sql, created_by, updated_by, created_at, updated_at);//上記で定義した変数を使用。
 	}
 
 }
